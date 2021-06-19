33d006a4f16c78902b214b12773f52d8dc768be6
LucasZeD <80793227+LucasZeD@users.noreply.github.com>
2021-06-19 07:51:58 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,33 +3,32 @@ const TMDB_ENDPOINT = 'https://api.themoviedb.org/3';
 const APIKEY = '31587d8c96bb5ef89c58f85a654d8806';
 const IMG_PREFIX = 'https://image.tmdb.org/t/p/w500';
 
-function carregaFilmes () {
-    xhr = new XMLHttpRequest ();
+function carregaFilmes() {
+    xhr = new XMLHttpRequest();
 
-    xhr.open ('GET', TMDB_ENDPOINT + '/movie/popular?api_key=' + APIKEY);
+    xhr.open('GET', TMDB_ENDPOINT + '/movie/popular?api_key=' + APIKEY);
     xhr.onload = exibeFilmes;
     xhr.send();
 }
 
-function pesquisaFilmes () {
-    let xhr = new XMLHttpRequest ();
+function pesquisaFilmes() {
+    let xhr = new XMLHttpRequest();
 
     //let query = document.getElementById('inputPesquisa').value;
     let query = document.getElementById('barra').value;
 
 
-    xhr.open ('GET', TMDB_ENDPOINT + '/search/movie?api_key=' + APIKEY + '&query=' + query);
+    xhr.open('GET', TMDB_ENDPOINT + '/search/movie?api_key=' + APIKEY + '&query=' + query);
     xhr.onload = exibeFilmes;
     xhr.send();
 }
 
+function exibeFilmes(evt) {
 
-function exibeFilmes (evt) {
-    
     //let data = JSON.parse (xhr.responseText);
     let textoHTML = '';
 
-    let filmes = JSON.parse (evt.target.responseText);
+    let filmes = JSON.parse(evt.target.responseText);
 
 
     //for (let i = 0; i < data.results.length; i++) {
@@ -38,22 +37,20 @@ function exibeFilmes (evt) {
         let dataLancamento = filmes.results[i].release_date;
         let sinopse = filmes.results[i].overview;
         let imagem = IMG_PREFIX + filmes.results[i].poster_path;
-
-
+        //NEW
+        let link = `https://www.themoviedb.org/movie/${filmes.results[i].id}?language=pt-BR`;
 
         textoHTML += `<div class="card col-md-4">
-            <img src="${imagem}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${nomeFilme}</h5>
-                <p class="card-data">${dataLancamento}</p>
-                <p class="card-text">${sinopse}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+        <img src="${imagem}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${nomeFilme}</h5>
+        <p class="card-data">${dataLancamento}</p>
+        <p class="card-text">${sinopse}</p>
+        <a target="_blank" href="${link}" class="btn btn-primary">Go somewhere</a>
+        </div>
         </div>`
-
-
+        //<a href="#" class="btn btn-primary">Go somewhere</a>
     }
-
     document.getElementById('tela').innerHTML = textoHTML;
 }
 
